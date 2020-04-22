9f80d2e83963f7841bc91f02bca1b12f96e961e1
bruce willis <fashun_vyctum@yahoo.com>
2020-04-22 01:31:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,6 +51,18 @@ class Owner
     self.cats.each {|cats| cats.mood = "happy"}
   end
   
-  def
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
   
 end
